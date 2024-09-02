879838289d73223bdaa981acefcf034b2c0c1417
MatAlumno <135235011+MatAlumno@users.noreply.github.com>
2024-09-02 17:01:45 -0300

    Update App.js
    no me salio nada profe lpm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
 
+  const [advice, setAdvice] = useState("")
   const [term, setTerm] = useState("")
+  const [res, setRes] = useState("")
 
   const handleTermChange = (event) => setTerm(event.target.value);
+  const submitRes = (event) => {
+    axios.get('https://api.adviceslip.com/advice/search/${term}')
+    .then(response => setRes(response.data.slip.advice))
+    .catch(error => console.error('Error fetching data:', error));
+  };
+
+  useEffect(() => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then(response => setAdvice(response.data.slip.advice))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
 
   return (
     <main>
@@ -15,8 +30,8 @@ function App() {
 
       <div>
         <h2>Obtener un consejo aleatorio</h2>
-        <button>Obtener</button>
-        <p className="result-box"></p>
+        <button >Obtener</button>
+        <p className="result-box">{advice} holaaaa</p>
       </div>
 
       <div>
@@ -24,7 +39,7 @@ function App() {
         <input type="text" onChange={handleTermChange} />
         <button>Enviar</button>
         <h3>Resultados de búsqueda:</h3>
-        <p className="result-box"></p>
+        <p className="result-box">{res}</p>
       </div>
 
     </main>
